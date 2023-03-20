8d4a9831ba21bbad8645b4c465e517a9d8800b9c
Chisom Oguine <oguinechisomtimothy@gmail.com>
2023-03-20 10:04:54 +0100

    node challenge 2

---
+++ index.js
@@ -0,0 +1,7 @@
+const { sum, calculator, sub } = require("./math");
+
+// calculator.multiply();
+
+console.log(calculator.multiply(3, 2));
+console.log(sum(4, 5));
+console.log(sub(4, 9));
---
+++ math.js
@@ -0,0 +1,10 @@
+const sum = (barca, madrid) => barca + madrid;
+const sub = (barca, madrid) => barca - madrid;
+
+const calculator = {
+  multiply: (p, q) => {
+    return p * q;
+  },
+};
+
+module.exports = { sum, calculator, sub };
